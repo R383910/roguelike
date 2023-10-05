c5be9868b2d072dd7082f5a4f61f51b4791fe2ba
R383910 <141141806+R383910@users.noreply.github.com>
2023-10-05 09:24:59 +0200

    deplacement
    ajout des déplacement du joueur

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        MovePlayer(horizontalMovement, verticalMovement);
+    }
+
+    void MovePlayer(float _horizontalMovement, float _verticalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, _verticalMovement);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+}
