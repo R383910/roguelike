6f0efc258acf7afaa40a38ebb1be67d08862c6f4
R383910 <141141806+R383910@users.noreply.github.com>
2024-01-08 17:37:44 +0100

    ennemie

---
+++ EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+    private float nextRandomMoveTime = 0f;
+    private bool isMovingRandomly = false;
+
+    void FixedUpdate()
+    {
+        if (!isMovingRandomly)
+        {
+            StartCoroutine(MoveRandomly());
+        }
+    }
+    
+    IEnumerator MoveRandomly()
+    {
+        isMovingRandomly = true;
+
+        float randomHorizontal = Random.Range(-1f, 1f);
+        float randomVertical = Random.Range(-1f, 1f);
+
+        MovePlayer(randomHorizontal, randomVertical);
+
+        yield return new WaitForSeconds(5f);
+
+        nextRandomMoveTime = Time.time + 5f;
+        isMovingRandomly = false;
+    }
+}
