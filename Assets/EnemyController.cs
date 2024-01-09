128abf4b700a5033c81e9860d94d9cb59647a1b7
R383910 <gabrielmessaoudi130@gmail.com>
2024-01-09 13:23:06 +0100

    ennemy ia update

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -2,58 +2,33 @@
 
 public class EnemyController : MonoBehaviour
 {
-    // Les données de l'ennemi
-    public EnemyData enemyData;
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
 
-    private void Start()
+    private float nextRandomMoveTime = 0f;
+    private bool isMovingRandomly = false;
+
+    void FixedUpdate()
     {
-        // Initialiser l'ennemi lors du démarrage
-        InitializeEnemy();
+        if (!isMovingRandomly)
+        {
+            StartCoroutine(MoveRandomly());
+        }
     }
-
-    private void InitializeEnemy()
+    
+    IEnumerator MoveRandomly()
     {
-        // Vérifier si les données de l'ennemi sont assignées
-        if (enemyData == null)
-        {
-            Debug.LogError("EnemyData n'est pas assigné à EnemyController !");
-            return;
-        }
+        isMovingRandomly = true;
 
-        // Vérifier si le modèle de l'ennemi est assigné
-        if (enemyData.enemyModel == null)
-        {
-            Debug.LogError("enemyModel n'est pas assigné dans EnemyData !");
-            return;
-        }
+        float randomHorizontal = Random.Range(-1f, 1f);
+        float randomVertical = Random.Range(-1f, 1f);
 
-        // Vérifier si le composant Animator est assigné
-        if (enemyData.anim == null)
-        {
-            Debug.LogError("AnimatorController n'est pas assigné dans EnemyData !");
-            return;
-        }
+        MovePlayer(randomHorizontal, randomVertical);
 
-        // Assurez-vous que le modèle de l'ennemi a le même parent que l'objet EnemyController
-        enemyData.enemyModel.transform.parent = transform;
+        yield return new WaitForSeconds(5f);
 
-        // Optionnellement, définir d'autres propriétés telles que la santé, la vitesse, les dégâts, etc.
-        // enemyData.enemyModel.GetComponent<YourEnemyScript>().SetHealth(enemyData.health);
-        // enemyData.enemyModel.GetComponent<YourEnemyScript>().SetSpeed(enemyData.speed);
-        // enemyData.enemyModel.GetComponent<YourEnemyScript>().SetDamage(enemyData.damage);
-    }
-
-    // Méthode pour jouer une animation spécifiée par son nom
-    public void PlayAnimation(string nomAnimation)
-    {
-        // Vérifier si le composant Animator est initialisé
-        if (enemyData.anim == null)
-        {
-            Debug.LogError("AnimatorController n'est pas initialisé !");
-            return;
-        }
-
-        // Jouer l'animation spécifiée en déclenchant l'état correspondant
-        enemyData.anim.Play(nomAnimation, 0, 0f);
+        nextRandomMoveTime = Time.time + 5f;
+        isMovingRandomly = false;
     }
 }
