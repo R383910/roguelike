e91aac6aaf93250173ee7819ad45e83441906707
R383910 <gabrielmessaoudi130@gmail.com>
2024-01-09 16:30:37 +0100

    enemy speed
    la vitesse de l'ennemie est renseigner grâce au ennemie Data

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyController : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
+    public EnemyData enemyData;
 
     private float nextRandomMoveTime = 0f;
     private bool isMovingRandomly = false;
+
+    private void Start()
+    {
+        moveSpeed = enemyData.speed;
+    }
 
     void FixedUpdate()
     {
@@ -22,8 +30,8 @@ public class EnemyController : MonoBehaviour
     {
         isMovingRandomly = true;
 
-        float randomHorizontal = Random.Range(-1f, 1f);
-        float randomVertical = Random.Range(-1f, 1f);
+        float randomHorizontal = Random.Range(-5f, 5f);
+        float randomVertical = Random.Range(-5f, 5f);
 
         MoveEnnemy(randomHorizontal, randomVertical);
 
